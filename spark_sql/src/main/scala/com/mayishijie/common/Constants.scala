51dca3e2f1597df1f9915459787c11cf2083667a
mayishijie <mayishijie2020@163.com>
2021-06-09 23:26:08 +0800

    rdd转df

--- spark_sql/src/main/scala/com/mayishijie/common/Constants.scala
+++ spark_sql/src/main/scala/com/mayishijie/common/Constants.scala
@@ -5,6 +5,7 @@ object Constants {
   // 文件路径
   val COMMON_PATH = "/Users/tty/IdeaProjects/spark/spark_pro/spark_sql/src/main/resources/"
   val JSON_PATH = COMMON_PATH + "test.json"
+  val PERSON_CSV_PATH = COMMON_PATH+"person.txt"
 
 
   // 文件格式
---
+++ spark_sql/src/main/scala/com/mayishijie/makeDF/RddToDF.scala
@@ -0,0 +1,47 @@
+package com.mayishijie.makeDF
+
+import com.mayishijie.common.Constants
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+object RddToDF {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("toDf").setMaster("local[2]")
+    val spark = SparkSession.builder().config(conf).getOrCreate()
+    val sc = spark.sparkContext
+
+    val rdd = sc.textFile(Constants.PERSON_CSV_PATH)
+
+    //手动转换DF
+    //convert2Df(spark,rdd)
+
+    // 通过样例类,生产常用
+    conver2DfByClass(spark,rdd)
+
+  }
+
+  /**手动转换DF*/
+  def convert2Df(spark:SparkSession,rdd:RDD[String]):Unit={
+    //导入的不是spark手动包
+    import spark.implicits._
+    rdd.map(line=>{
+      val filds = line.split(",")
+      (filds(0),filds(1).trim.toInt)
+    }).toDF().show()
+  }
+
+  /**通过样例类来转化*/
+  def conver2DfByClass(spark:SparkSession,rdd: RDD[String]):Unit={
+    import spark.implicits._
+    rdd.map(
+      line=>{
+        val filds = line.split(",")
+        Person(filds(0),filds(1).trim.toInt)
+      }
+    ).toDF.show()
+  }
+
+}
+
+case class Person(name:String,age:Int)
